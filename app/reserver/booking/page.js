000e6f57e834737106091b7b196c8c2b8178fdb2
Hassihub <hasnain_ghossie@hotmail.com>
2026-01-29 16:02:00 +0100

    Move Booking page to client component to fix prerender error

--- app/reserver/booking/page.js
+++ app/reserver/booking/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import BookingClient from "./BookingClient";
 
 export default function Page() {
-  return <BookingClient />;
+  return (
+    <Suspense fallback={<div>Laster reservasjonssiden...</div>}>
+      <BookingClient />
+    </Suspense>
+  );
 }
