a3534ee8686db9178e1932a9747540828f0d8f34
Sask1603 <sigurdpask@gmail.com>
2026-02-04 10:58:32 +0100

    gjort at "utforsk" refererer til riktig side

--- appfrittfram/app/page.js
+++ appfrittfram/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main style={{ padding: 0 }}>
         <div className="box-container">
-          <Link href="/utforsk" className="info-box">
+          <Link href="/explore" className="info-box">
             <Image
               src="/images/Explore.jpg"
               alt="Utforsk"
