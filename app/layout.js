6c995ed7fed8323b22fe44c0b931754ee3d9aa6d
Sask1603 <sigurdpask@gmail.com>
2026-03-03 15:01:11 +0100

    Fjernet endringene fra global og layout

--- app/layout.js
+++ app/layout.js
@@ -19,25 +19,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="no">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          minHeight: "100vh",
-          display: "flex",
-          flexDirection: "column",
-        }}
->
+      <body style={{ margin: 0, padding: 0 }}>
         <TopBar />
 
-        <main style={{ flex: 1 }}>
-          {children}
-        </main>
+        {children}
 
         <footer className="footer">
           Dette er en footer som ligger over bildene
         </footer>
-        </body>
+      </body>
     </html>
   );
 }
