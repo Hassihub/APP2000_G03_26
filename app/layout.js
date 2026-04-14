02573c2f0abb5cdb4e6a3504b049a352fb5d51c9
Hassihub <hasnain_ghossie@hotmail.com>
2026-04-14 15:08:11 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import AppShell from "./components/AppShell";
 import { LanguageProvider } from "./components/LanguageProvider";
@@ -21,6 +22,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="no">
+      <head>
+        <Script
+          id="ms-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wbklyogxkr");
+            `,
+          }}
+        />
+      </head>
       <body style={{ margin: 0, padding: 0 }}>
         <div className="school-project-banner">
           Dette er et skoleprosjekt
