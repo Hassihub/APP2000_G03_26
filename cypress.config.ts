6420269f0a5f349b588af9fde9b52b35c88afb68
chh303 <hjortlandc@gmail.com>
2026-04-14 13:13:06 +0200

    testing files and reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,12 +3,18 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "z85gcg",
   allowCypressEnv: false,
-
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "FritFram Tests",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
   e2e: {
     baseUrl: "http://localhost:3000",
     viewportWidth: 1280,
     viewportHeight: 800,
-    video: false,
+    video: true,
     screenshotOnRunFailure: true,
     defaultCommandTimeout: 8000,
     excludeSpecPattern: [
@@ -16,10 +22,9 @@ export default defineConfig({
       "cypress/e2e/2-advanced-examples/**",
     ],
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
-
   component: {
     devServer: {
       framework: "next",
